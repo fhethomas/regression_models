60f6498739b837a81dad3e6af4a1663227a5da79
= <fhethomas@github.com>
2018-06-28 16:53:54 -0700

    Created module files

---
+++ __init__.py
@@ -0,0 +1,28 @@
+
+# coding: utf-8
+
+# In[2]:
+
+"""
+    Search_Image_Scraper
+    The purpose of this is to pull images off of Google Image Search.
+    
+    Created by Frank Thomas 05/08/2018
+    
+    Import module and then select next stage.
+    Currently there is only an Image_Scraper
+    
+    Example
+    
+    Import SearchImageScraper
+    
+    test_Image_Scraper = SearchImageScraper.Image_Scraper()
+"""
+
+try:
+    from regression_models import Linear_regression
+except:
+    from .regression_models import Linear_regression
+
+
+
